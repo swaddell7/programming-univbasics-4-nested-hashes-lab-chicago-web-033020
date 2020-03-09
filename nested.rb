af5b9c99311dc02cd37f1728148f57164feb06a1
Stefani Waddell <stefani.waddell@hotmail.com>
2020-03-09 20:11:59 +0000

    Done.

--- nested.rb
+++ nested.rb
@@ -16,6 +16,8 @@ def hopper
       }
     }
 
+programmer_hash[:grace_hopper]
+
 end
 
 def alan_kay_is_known_for
@@ -37,6 +39,8 @@ def alan_kay_is_known_for
       }
     }
 
+programmer_hash[:alan_kay][:known_for]
+
 end
 
 def dennis_ritchies_language
@@ -55,6 +59,8 @@ def dennis_ritchies_language
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages][0]
 
 end
 
@@ -82,6 +88,11 @@ def adding_matz
       }
     }
 
+programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LISP", "C"]
+}
+
+programmer_hash
+
 end
 
 def changing_alan
@@ -103,6 +114,12 @@ def changing_alan
         :languages => ["C"]
       }
     }
+    
+    alans_new_info = "GUI"
+    
+    programmer_hash[:alan_kay][:known_for] = alans_new_info
+    
+    programmer_hash
 
 end
 
@@ -125,5 +142,9 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
+
+programmer_hash[:dennis_ritchie][:languages].push("Assembly")
+
+programmer_hash
 
 end
